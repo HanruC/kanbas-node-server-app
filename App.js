f28b79ee3ce4f86517d5b29bb01a3e9fa87bc9a0
HanruC <chen.hanru@northeastern.edu>
2024-04-14 02:12:45 -0400

    a6

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignments/routes.js";
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
 import session from "express-session";
@@ -13,31 +12,43 @@ import session from "express-session";
 const app = express();
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
 mongoose.connect(CONNECTION_STRING);
-app.use(
-    cors({      credentials: true,
-        origin: 'process.env.FRONTEND_URL'}
-
-    )
-    );
+// const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => {
+    console.log('Connected to MongoDB');
+  }).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+  app.use(
+    cors({
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+       //origin: "http://localhost:3000",
+    })
+  );
+// app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(express.json());
 UserRoutes(app);
 ModuleRoutes(app);
 Lab5(app);
 Hello(app);
+AssignmentRoutes(app);
 
 
 CourseRoutes(app);
 const sessionOptions = {
-    secret: "any string",
-    resave: false,
-    saveUninitialized: false,
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
 };
 if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-        sameSite: "none",
-        secure: true,
-    };
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
 }
 app.use(session(sessionOptions));
 app.listen(process.env.PORT || 4000);
