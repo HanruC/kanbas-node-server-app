eca7cff6b73c60189edff49f2a35b1ea2dd8e772
HanruC <chen.hanru@northeastern.edu>
2024-04-13 23:00:44 -0400

    A6

--- App.js
+++ App.js
@@ -5,13 +5,39 @@ import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignments/routes.js";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
 
 const app = express();
-app.use(cors());
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
+app.use(
+    cors({      credentials: true,
+        origin: 'process.env.FRONTEND_URL'}
+
+    )
+    );
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
-CourseRoutes(app);
-AssignmentRoutes(app);
 Lab5(app);
 Hello(app);
+
+
+CourseRoutes(app);
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
 app.listen(process.env.PORT || 4000);
