d7cb2d353440859dfffaf5c692060d38fb82f82a
HanruC <chen.hanru@northeastern.edu>
2024-04-14 21:35:29 -0400

    deploy check

--- App.js
+++ App.js
@@ -13,11 +13,12 @@ import Lab5 from "./lab5.js";
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/kanbas");
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
 
 app.use(cors({
   credentials: true,
-  origin: process.env.FRONTEND_URL,
+  origin: "a6--silly-sundae-f73064.netlify.app",
 }));
 
 const sessionOptions = {
@@ -31,7 +32,7 @@ if (process.env.NODE_ENV !== 'development') {
   sessionOptions.cookie = {
     sameSite: 'none',
     secure: true,
-    domain: process.env.HTTP_SERVER_DOMAIN,
+    // domain: process.env.HTTP_SERVER_DOMAIN,
   };
 }
 
